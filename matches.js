5a4c476dc2b27b363ae52bb948debf91fd72f9bd
skdhameliya <53290891+skdhameliya@users.noreply.github.com>
2020-09-27 15:45:02 +0530

    Add files via upload

---
+++ matches.js
@@ -0,0 +1,28 @@
+
+var myMain = {
+
+	"myMatch" : [
+		{
+			"col1":"data11",
+			"col2":"data12data12data12",
+			"col3":"data13",
+			"col4":"data14"
+		},
+		{
+			"col1":"data21",
+			"col2":"data22",
+			"col3":"data23data12",
+			"col4":"data24"
+		},
+		{
+			"col1":"data31",
+			"col2":"data32data12",
+			"col3":"data33",
+			"col4":"data34"
+		}
+	]
+
+};
+
+
+// =======
